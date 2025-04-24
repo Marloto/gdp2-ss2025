d00b7ac1572139339d04f63ffaf6ad5517c6da1e
Marloto <marloto@codestack.de>
2025-04-24 10:38:55 +0200

    Added missing files from group 01 (27.03 and 03.04)

---
+++ src/group01/lecture20250327/AudioFile.java
@@ -0,0 +1,22 @@
+package group01.lecture20250327;
+
+public class AudioFile {
+	// Strg/Control + Leertaste -> Auto Complete
+	// -> main od. syso erzeugt entsprechende Templates
+	// -> in IntelliJ psvm, sout
+	// Strg / Cmd gedrückt halten
+	// Doppelklick auf Tabs erzeugt max-ansicht
+	// per Drag&Drop Tabs ziehen um Ansichten zu teilen
+	// Strg / Cmd + 1 -> Quick Fix, Cursor muss dem Fehler sein
+	// Strg / Cmd + O -> Outline als Overlay (Alternative zum View)
+	// Strg / Cmd + 7 -> Toggle Line Comment
+	// Shift + Strg / Cmd + F -> Auto Format
+	// Strg / Cmd + L -> Go to Line
+	// Strg / Cmd + D -> Delete Line
+	// Alt / Option + Strg / Cmd + Pfeil hoch / runter -> Duplizieren
+	// Alt / Option + Pfeil hoch / runter -> Move Line
+
+	public String getPathname() {
+		return "Test2";
+	}
+}
